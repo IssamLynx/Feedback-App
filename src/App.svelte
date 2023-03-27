76ab45b54a452bb77e3b23d778d2acebc0f2994a
IssamLynx <hm_bakha@esi.dz>
2023-03-27 17:48:11 +0200

    Added Card, FeedbackItem and FeedbackList

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte'
+</script>
+
+<main class="container">
+
+	<FeedbackList />
+</main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import {FeedbackStore} from '../stores'
+  import FeedbackItem from './FeedbackItem.svelte'
+
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <FeedbackItem item={fb} />
+{/each}
+Footer
+© 2023 GitHub, Inc.
