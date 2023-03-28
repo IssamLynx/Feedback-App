251aaeb926305fce228a1e1bc0f3e6cc3ca018c7
IssamLynx <hm_bakha@esi.dz>
2023-03-28 11:10:29 +0200

    Added FeedbackStats

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
 	import FeedbackList from './components/FeedbackList.svelte'
+  import FeedbackStats from './components/FeedbackStats.svelte';
 </script>
 
 <main class="container">
-
+	<FeedbackStats />
 	<FeedbackList />
 </main>
